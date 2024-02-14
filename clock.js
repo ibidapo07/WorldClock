ac817128c1fe88234ecb4265587eab244a71301b
ibidapo07 <fatogunibidapo@gmail.com>
2024-02-14 08:53:29 +0100

    Seconds hand spinning animation

--- clock.js
+++ clock.js
@@ -1,13 +1,24 @@
 const today = new Date();
 const hour = today.getUTCHours();
 const minutes = today.getMinutes();
-const seconds = today.getSeconds();
+var seconds = today.getSeconds();
 
-// const hour_hand = document.getElementById("hour");
-// const minute_hand = document.getElementsById("minute-default");
+const hour_hand = document.getElementById("hour");
+const minute_hand = document.getElementsById("minute-default");
+const seconds_hand = document.getElementsById("seconds");
 
 function moveSeconds() {
-  //   alert();
-  let seconds_hand = document.getElementsByClassName("seconds-hand");
-  seconds_hand.style["background-color"] = "red";
+  let seconds_hand = document.getElementById("seconds-hand");
+  seconds_hand.animate(
+    {
+      transform: `rotateZ(${(seconds / 60) * 3600}deg)`,
+    },
+    {
+      duration: 500000,
+      iterations: 3,
+    }
+  );
+  let text = document.getElementById("text");
+
+  text.innerHTML = (seconds / 60) * 360;
 }
