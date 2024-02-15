5ac5a380e7ed0db368f33d23c958350283f55022
ibidapo07 <fatogunibidapo@gmail.com>
2024-02-15 18:47:21 +0100

    All hand spin now with simple dom manipulation

--- clock.js
+++ clock.js
@@ -1,24 +1,36 @@
-const today = new Date();
-const hour = today.getUTCHours();
-const minutes = today.getMinutes();
-var seconds = today.getSeconds();
+// // const hour = today.getUTCHours();
+// // const minutes = today.getMinutes();
 
-const hour_hand = document.getElementById("hour");
-const minute_hand = document.getElementsById("minute-default");
-const seconds_hand = document.getElementsById("seconds");
+// const hour_hand = document.getElementById("hour");
+// const minute_hand = document.getElementsById("minute-default");
+// const seconds_hand = document.getElementsById("seconds");
 
 function moveSeconds() {
-  let seconds_hand = document.getElementById("seconds-hand");
-  seconds_hand.animate(
-    {
-      transform: `rotateZ(${(seconds / 60) * 3600}deg)`,
-    },
-    {
-      duration: 500000,
-      iterations: 3,
-    }
-  );
-  let text = document.getElementById("text");
+  const today = new Date();
 
-  text.innerHTML = (seconds / 60) * 360;
+  const seconds = today.getUTCSeconds();
+  const minute = today.getUTCMinutes();
+  const hour = today.getUTCHours();
+
+  const seconds_hand = document.getElementById("seconds-hand");
+  const minute_hand = document.getElementById("minute-hand-default");
+  const hour_hand = document.getElementById("hour-hand");
+
+  seconds_hand.style.transform = `rotateZ(${seconds * 6 - 180}deg)`;
+  minute_hand.style.transform = `rotateZ(${minute * 6 - 180}deg)`;
+  hour_hand.style.transform = `rotateZ(${hour * 30 - 180}deg)`;
 }
+
+function loadPageAccurately() {
+  moveSeconds();
+}
+
+function startclock() {
+  let intvl;
+
+  if (!intvl) {
+    intvl = setInterval(moveSeconds, 1000);
+  }
+}
+
+document.onload(loadPageAccurately());
